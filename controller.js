583e68b7b5ab7325c10926e9c15be69b210e1495
AlexanderRinkowski <47833732+AlexanderRinkowski@users.noreply.github.com>
2024-09-30 01:26:26 +0200

    MVC seemingly working

--- controller.js
+++ controller.js
@@ -1,144 +1,255 @@
 // controller.js
 import { Card, GameModel } from './model.js';
 
-const updateDOM = {
-    cardToHTML(card) {
-        const cardElement = document.createElement('div');
-        cardElement.id = `card-${card.id}`;
-        const playerCSS = card.owner === 'player1' ? 'player-one' : 'player-two'; 
-        cardElement.className = `card ${playerCSS}`;
-        cardElement.innerHTML = `Strength: ${card.strength}<br>Type: ${card.cardType}`;
-        cardElement.dataset.strength = card.strength;
-        cardElement.dataset.cardType = card.cardType;
-        cardElement.draggable = true;
-        return cardElement;
-    },
-    renderHand(handModel) {
-        const handCardContainers = {
-            player1: document.getElementById('hand-cards-player1'),
-            player2: document.getElementById('hand-cards-player2')
-        };
-        for (const playerIndex in handModel) {
-            let cards = handModel[playerIndex];
-            cards.forEach(card => { // create card divs and add to container
-                const cardElement = this.cardToHTML(card);
-                handCardContainers[playerIndex].appendChild(cardElement);
-            })
-        }
-    },
-    renderBoard(boardModel) {
-        const rowCardContainers = {
-            fire: document.getElementById('fire-cards'),
-            water: document.getElementById('water-cards'),
-            air: document.getElementById('air-cards'),
-            earth: document.getElementById('earth-cards')
-        };        
-        for (const rowIndex in boardModel) {
-            console.log(rowIndex)
-            let cards = boardModel[rowIndex];
-            cards.forEach(card => { // create card divs and add to container
-                const cardElement = this.cardToHTML(card);
-                console.log(card)
-                rowCardContainers[rowIndex].appendChild(cardElement);
-            })
-        }
-    }
-}
+let model = new GameModel();
 
-const dragUtils = {
-    dragStart(e) {
-        e.dataTransfer.setData('text/plain', e.target.id); // set element id for transfer
-    },
-    dragOver(e) {
-        e.preventDefault();
-    },
-    drop(e, validTargetIds) { // drop the card if its on a row
-        e.preventDefault();
-        const elementId = e.dataTransfer.getData('text/plain'); // data was set to card.id
-        const element = document.getElementById(elementId);
-        console.log(e.target.id)
-        const hasValidId = validTargetIds.some(id => e.target.id === id);
-        hasValidId ? document.getElementById(e.target.id).appendChild(element) : console.log("Not a valid drop target.");
-    }
-}
+// ViewModelMapping ? 
 
-const touchUtils = {
-    touchStart(e) { 
-        e.preventDefault();
-        const element = e.target;
-        if (element.draggable) { // if movable by drag / drop then also by touch
-            // bring to front
-            element.style.position = 'absolute';
-            element.style.zIndex = '1000'; 
-            // update position
-            const touch = e.targetTouches[0];
-            element.style.left = `${touch.pageX - element.offsetWidth / 2}px`;
-            element.style.top = `${touch.pageY - element.offsetHeight / 2}px`;
-        } 
-    },
-    touchMove(e) { // keep dragging 
-        e.preventDefault(); 
-        const element = e.target;
-        if (element.draggable) { // if movable by drag / drop then also by touch
-            // update card position
-            const touch = e.targetTouches[0];
-            element.style.left = `${touch.pageX - element.offsetWidth / 2}px`;
-            element.style.top = `${touch.pageY - element.offsetHeight / 2}px`;
+const userInput = {
+    moveCardDragging: {
+        dragStart(e) {
+            e.dataTransfer.setData('text/plain', e.target.id); // set element id for transfer
+        },
+        dragOver(e) {
+            e.preventDefault();
+        },
+        drop(e) { // drop the card if its on a row
+            e.preventDefault();
+            const cardId = e.dataTransfer.getData('text/plain'); // data was set to card.id
+            const isValidMove = inputValidation.verifyCardMove(cardId, e.target.id);
+            if(!isValidMove){
+                alert("Not a valid target.");
+            }
         }
     },
-    touchEnd(e, validTargetIds) { // stop dragging (reset positioning) & if drag stopped over row add card
-        e.preventDefault();
-        const element = e.target;
-        if (element.draggable) {
-            // reset that card was brought to front
-            element.style.position = '';
-            element.style.zIndex = '';
-            // Check if the card is being dropped onto a valid row
-            const touch = e.changedTouches[0];
-            const dropTargets = document.elementsFromPoint(touch.clientX, touch.clientY);
-            for (let target of dropTargets) {
-                const hasValidId = validTargetIds.some(id => target.id === id);
-                if (hasValidId) {
-                    document.getElementById(target.id).appendChild(element);
-                    break;
+    moveCardTouch: {
+        touchStart(e) { 
+            e.preventDefault();
+            const element = e.target;
+            if (element.draggable) { // if movable by drag / drop then also by touch
+                // bring to front
+                element.style.position = 'absolute';
+                element.style.zIndex = '1000'; 
+                // update position
+                const touch = e.targetTouches[0];
+                element.style.left = `${touch.pageX - element.offsetWidth / 2}px`;
+                element.style.top = `${touch.pageY - element.offsetHeight / 2}px`;
+            } 
+        },
+        touchMove(e) { // keep dragging 
+            e.preventDefault(); 
+            const element = e.target;
+            if (element.draggable) { // if movable by drag / drop then also by touch
+                // update card position
+                const touch = e.targetTouches[0];
+                element.style.left = `${touch.pageX - element.offsetWidth / 2}px`;
+                element.style.top = `${touch.pageY - element.offsetHeight / 2}px`;
+            }
+        },
+        touchEnd(e) { // stop dragging (reset positioning) & if drag stopped over row add card
+            e.preventDefault();
+            const element = e.target;
+            if (element.draggable) {
+                // reset that card was brought to front
+                element.style.position = '';
+                element.style.zIndex = '';
+                // Check if the card is being dropped onto a valid row
+                const touch = e.changedTouches[0];
+                const dropTargets = document.elementsFromPoint(touch.clientX, touch.clientY);
+                let isValidMove = false;
+                for (let target of dropTargets) {
+                    isValidMove = inputValidation.verifyCardMove(element.id, target.id);
+                    if(isValidMove){
+                        break;
+                    }
                 }
+                if (!isValidMove) { alert("Not a valid target."); }
+            }
+        }
+    },
+    buttons: {
+        endTurnButtonPlayer1() {
+            const isVerified = inputValidation.verifyEndTurn("player1");
+            if (!isVerified) { 
+                alert("End turn was not legal."); 
+            }
+        },
+        endTurnButtonPlayer2() {
+            const isVerified = inputValidation.verifyEndTurn("player2");
+            if (!isVerified) { 
+                alert("End turn was not legal.");
             }
         }
     }
 }
 
-let currentPlayerCSS = 'player-one'; 
-let currentOpponentCSS = 'player-two';
-
-document.addEventListener('DOMContentLoaded', () => {
-    const model = new GameModel();
-
-    const c1 = new Card("1", 1, "warrior", "player1");
-    const c2 = new Card("2", 3, "wizzard", "player1");
-    model.addCardToHand("player1", c1);
-    model.addCardToHand("player1", c2);
-    model.moveCardFromHandToBoard("player1", 1, "water");
-
-    function render() {
-        updateDOM.renderHand(model.hand);
-        updateDOM.renderBoard(model.board);
-        // add eventBinding
-        const cards = document.querySelectorAll('.card');
-        cards.forEach(card => { 
-            card.addEventListener('dragstart', dragUtils.dragStart);
-            card.addEventListener('touchstart', touchUtils.touchStart, { passive: false }); // passive: false to prevent default behavior
-            card.addEventListener('touchmove', touchUtils.touchMove, { passive: false });
-            card.addEventListener('touchend', (e) => touchUtils.touchEnd(e, ["water-cards", "fire-cards", "air-cards", "earth-cards"]), { passive: false }); // touch end works different from drop - see function touchEnd
-        });
-        
-        // enable drops
-        const rows = document.querySelectorAll('.row');
-        rows.forEach(row => { 
-            row.addEventListener('dragover', dragUtils.dragOver);
-            row.addEventListener('drop', (e) => dragUtils.drop(e, ["water-cards", "fire-cards", "air-cards", "earth-cards"]));
-        });
+const inputValidation = { // (updateModel)
+    verifyCardMove(cardElementId, targetElementId) {
+        const cardId = cardElementId.split('-')[1];
+        const cardModel = model.cardLookup[cardId];
+        const targetModelId = targetElementId.split('-')[0];
+        const isValidMove = this.modelRequests.cardMoveRequest(cardModel, targetModelId)
+        return isValidMove
+    },
+    verifyEndTurn(player){
+        const isVerified = this.modelRequests.endTurnRequest(player);
+        return isVerified;
+    },
+    modelRequests: {
+        cardMoveRequest(cardModel, targetModelId) {
+            const wasCardplayed = model.moveCard(cardModel, targetModelId);
+            return wasCardplayed;
+        },
+        endTurnRequest(player) {
+            const isVerified = model.endTurn(player);
+            return isVerified;
+        }
     }
+}
 
-    render(); 
+const updateView = {
+    renderFull() {
+        // pure html manipulation
+        this.staticDomUpdate.renderHandCards();
+        this.staticDomUpdate.renderBoardCards();
+        this.staticDomUpdate.renderPlayerInfo();
+        this.staticDomUpdate.renderBoardInfo();
+        
+        // add event binding to all cards 
+        const cards = document.querySelectorAll('.card');
+        cards.forEach(cardElement => {
+            const cardId = cardElement.id.split('-')[1];
+            const cardModel = model.cardLookup[cardId]
+            if(cardModel.owner === model.currentPlayer) { 
+                cardElement.setAttribute("draggable", "true");
+                this.eventBinding.addCardListeners(cardElement);
+            }
+        });
 
+        // event binding for rows
+        const rows = document.querySelectorAll('.cards');
+        rows.forEach(row => { 
+            if(["water-cards", "fire-cards", "air-cards", "earth-cards"].includes(row.id)){
+                this.eventBinding.addRowListeners(row);
+            }
+        });
+
+        // other event binding 
+        this.eventBinding.addButtonListeners()
+    },
+    staticDomUpdate: {
+        cardToHTML(card) {
+            const cardElement = document.createElement('div');
+            cardElement.id = `card-${card.id}`;
+            const playerCSS = card.owner === 'player1' ? 'player-one' : 'player-two'; 
+            cardElement.className = `card ${playerCSS}`;
+            cardElement.innerHTML = `Strength: ${card.strength}<br>Type: ${card.cardType}`;
+            cardElement.dataset.strength = card.strength;
+            cardElement.dataset.cardType = card.cardType;
+            return cardElement;
+        },
+        renderHandCards() {
+            const handModel = model.hand;
+            const handCardContainers = {
+                // Mapping: id in model: html Element
+                player1: document.getElementById('player-one-cards-hand'),
+                player2: document.getElementById('player-two-cards-hand')
+            };
+            for (const playerIndex in handModel) {
+                // reset view
+                handCardContainers[playerIndex].innerHTML = '';
+                // render cards according to model
+                let cards = handModel[playerIndex];
+                cards.forEach(card => { 
+                    const cardElement = this.cardToHTML(card);
+                    handCardContainers[playerIndex].appendChild(cardElement);
+                })
+            }
+        },
+        renderBoardCards() {
+            const boardModel = model.board;
+            const rowCardContainers = {
+                // Mapping: id in model: html Element
+                fire: document.getElementById('fire-cards'),
+                water: document.getElementById('water-cards'),
+                air: document.getElementById('air-cards'),
+                earth: document.getElementById('earth-cards')
+            };        
+            // create card div for each card in model
+            for (const rowIndex in boardModel) {
+                // reset view
+                rowCardContainers[rowIndex].innerHTML = '';
+                // render cards according to model
+                let cards = boardModel[rowIndex];
+                cards.forEach(card => { 
+                    const cardElement = this.cardToHTML(card);
+                    rowCardContainers[rowIndex].appendChild(cardElement);
+                })
+            }
+        },
+        renderPlayerInfo() {
+            const infoModel = model.info;
+            const rowCardContainers = {
+                player1Coin: document.getElementById('player-one-info-coin'),
+                player2Coin: document.getElementById('player-two-info-coin'),
+                player1Magic: document.getElementById('player-one-info-magic'),
+                player2Magic: document.getElementById('player-two-info-magic')
+            };        
+            for (const index in infoModel) {
+                const modelValue = infoModel[index];
+                const htmlElement = rowCardContainers[index];
+                htmlElement.innerHTML = modelValue;
+            }
+        },
+        renderBoardInfo() {
+            const infoModel = model.rowOwner;
+            const rowCardContainers = {
+                fire: document.getElementById('fire-info'),
+                water: document.getElementById('water-info'),
+                air: document.getElementById('air-info'),
+                earth: document.getElementById('earth-info')
+            };        
+            for (const index in infoModel) {
+                const modelValue = infoModel[index];
+                const rowHeader = rowCardContainers[index];
+                if (modelValue === "player1") { rowHeader.style.backgroundColor = 'antiquewhite'; }
+                else if (modelValue === "player2") { rowHeader.style.backgroundColor = 'lightblue'; }
+                else { rowHeader.style.backgroundColor = ''; }
+            }
+        }
+    },
+    eventBinding: {
+        addCardListeners(card){
+            // allow drag start & touch start
+            card.addEventListener('dragstart', userInput.moveCardDragging.dragStart);
+            card.addEventListener('touchstart', userInput.moveCardTouch.touchStart, { passive: false }); // passive: false to prevent default behavior
+            card.addEventListener('touchmove', userInput.moveCardTouch.touchMove, { passive: false });
+            card.addEventListener('touchend', userInput.moveCardTouch.touchEnd, { passive: false }); // touch end works different from drop - see function touchEnd
+        },
+        addRowListeners(row){
+            // enable drops
+            row.addEventListener('dragover', userInput.moveCardDragging.dragOver);
+            row.addEventListener('drop', userInput.moveCardDragging.drop);
+        },
+        addButtonListeners(){
+            document.getElementById("player-one-end-turn").addEventListener('click', userInput.buttons.endTurnButtonPlayer1);
+            document.getElementById("player-two-end-turn").addEventListener('click', userInput.buttons.endTurnButtonPlayer2);
+            const opponentButtonId = model.currentPlayer === "player1" ? "player-two-end-turn": "player-one-end-turn";
+            const playerButtonId = model.currentPlayer === "player1" ? "player-one-end-turn" : "player-two-end-turn";
+            document.getElementById(opponentButtonId).setAttribute('disabled', true);  
+            if (document.getElementById(playerButtonId).hasAttribute('disabled')) {
+                document.getElementById(playerButtonId).removeAttribute('disabled');
+            }
+        }
+    }
+}
+
+// On initial load
+document.addEventListener('DOMContentLoaded', () => {
+    // Subscribe to model for render updates
+    model.onModelChanged(() => {
+        updateView.renderFull(); 
+    });
+    // initital render
+    model.start();
 });
--- model.js
+++ model.js
@@ -1,3 +1,107 @@
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+class EventEmitter {
+    constructor() {
+        this.events = {};
+    }
+
+    on(event, listener) {
+        if (!this.events[event]) {
+            this.events[event] = [];
+        }
+        this.events[event].push(listener);
+    }
+
+    emit(event, data) {
+        if (this.events[event]) {
+            this.events[event].forEach(listener => listener(data));
+        }
+    }
+}
+
+const CombatHelper = {
+    startCombat(gameModel) {
+        for (const rowIndex in gameModel.board) {
+            if (gameModel.board[rowIndex].length <= 1) { continue; }
+            // Wizard combat
+            gameModel = this.WizzardCombat.resolveWizardCombat(gameModel, rowIndex);
+            // Close combat
+            gameModel = this.CloseCombat.resolveCloseCombat(gameModel, rowIndex);
+        }
+        return gameModel;
+    },
+    CloseCombat: {
+        resolveCloseCombat(gameModel, rowIndex) {
+            let cards = gameModel.board[rowIndex];
+            const defender = cards[0].owner;
+        
+            let defenderCards = cards.filter(card => card.owner === defender);
+            let attackerCards = cards.filter(card => card.owner !== defender);
+        
+            let defenderCard = defenderCards[defenderCards.length - 1]; 
+            let attackerCard = attackerCards[0]; 
+
+            while (defenderCards.length > 0 && attackerCards.length > 0) {
+                defenderCard = defenderCards[defenderCards.length - 1]; 
+                attackerCard = attackerCards[0]; 
+        
+                // Store original strengths
+                const originalDefenderStrength = defenderCard.strength;
+                const originalAttackerStrength = attackerCard.strength;
+        
+                // Apply damage
+                defenderCard.strength -= originalAttackerStrength;
+                attackerCard.strength -= originalDefenderStrength;
+        
+                // Filter out defeated cards
+                defenderCards = defenderCards.filter(card => card.strength > 0);
+                attackerCards = attackerCards.filter(card => card.strength > 0);
+        
+                // Update the original cards array
+                gameModel.board[rowIndex] = [...defenderCards, ...attackerCards];
+
+                gameModel.updateGameState() // notify the view about changes
+            }
+            return gameModel;
+        }               
+    },
+    WizzardCombat: {
+        resolveWizardCombat(gameModel, rowIndex) {
+            let cards = gameModel.board[rowIndex];
+            // Determine defender and attacker cards
+            let defender = cards[0].owner;
+
+            // Filter out cards for both players directly from the board
+            let defenderCards = cards.filter(card => card.owner === defender);
+            let attackerCards = cards.filter(card => card.owner !== defender);
+
+            // Calculate wizard attack value
+            const defenderWizardAttack = this.calculateWizardAttack(defenderCards);
+            const attackerWizardAttack = this.calculateWizardAttack(attackerCards);
+            // Apply wizard attack value   
+            defenderCards.forEach(card => { card.strength -= attackerWizardAttack; });
+            attackerCards.forEach(card => { card.strength -= defenderWizardAttack; });
+            
+            gameModel.board[rowIndex] = cards.filter(card => card.strength >= 0);
+
+            gameModel.updateGameState() 
+            return gameModel; 
+        },
+
+        calculateWizardAttack(cards) {
+            let wizardAttack = 0;
+            cards.forEach(card => { 
+                if (card.cardType === 'wizard') { wizardAttack = Math.max(wizardAttack, card.strength); }
+            });
+            return wizardAttack;
+        },
+    }
+};
+
+
+
 export class Card {
     constructor(id, strength, cardType, owner) {
         this.id = id;
@@ -9,6 +113,14 @@ export class Card {
 
 export class GameModel {
     constructor() {
+        this.currentPlayer = null; 
+        this.cardCount = null;
+        this.info = {
+            player1Coin: 10,
+            player2Coin: 10,
+            player1Magic: 5,
+            player2Magic: 5,
+        };
         this.hand = {
             player1: [],
             player2: []
@@ -19,31 +131,154 @@ export class GameModel {
             air: [],
             earth: []
         };
+        this.rowOwner = {
+            fire: "",
+            water: "",
+            air: "",
+            earth: ""
+        };
+        this.cardLookup = {};
+        this.eventEmitter = new EventEmitter();
     }
 
-    addCardToHand(owner, card) {
+    onModelChanged(listener) {
+        this.eventEmitter.on('modelChanged', listener);
+    }
+
+    updateGameState() {
+        this.eventEmitter.emit('modelChanged', this); // Notify listeners
+    }
+
+    _generateRandomCard() {
+        const cardTypes = ["warrior", "wizard", "farmer", "apprentice"];
+        const owners = ["player1", "player2"];
+    
+        // Generate random card properties
+        const id = String(this.cardCount);
+        this.cardCount++;
+        const strength = Math.floor(Math.random() * 9) + 1; // Strength between 1 and 9
+        const cardType = cardTypes[Math.floor(Math.random() * cardTypes.length)];
+        let owner = owners[Math.floor(Math.random() * owners.length)];
+    
+        // Create and return a new Card instance
+        const newCard =  new Card(id, strength, cardType, owner);
+        this.cardLookup[id] = newCard;
+        return newCard;
+    }
+
+    _switchCurrentPlayer() {
+        this.currentPlayer = this.currentPlayer === "player1" ? "player2" : "player1";
+    }
+
+    _addCardToHand(owner, card) {
+        card.owner = owner;
         this.hand[owner].push(card);
     }
 
-    getCardFromHand(owner, cardIndex) {
+    _getCardFromHand(owner, cardIndex) {
         const card = this.hand[owner][cardIndex];
         this.hand[owner].splice(cardIndex, 1);
         return card;
     }
 
-    addCardToBoard(row, card) {
+    _addCardToBoard(row, card) {
         this.board[row].push(card);
     }
     
-    getCardFromBoard(row, cardIndex) {
+    _getCardFromBoard(row, cardIndex) {
         const card = this.board[row][cardIndex];
         this.board[row].splice(cardIndex, 1);
         return card;
     }
 
-    moveCardFromHandToBoard(owner, cardIndex, row) {
-        const card = this.getCardFromHand(owner, cardIndex);
-        this.addCardToBoard(row, card);
+    _moveCardFromHandToBoard(owner, cardIndex, row) {
+        const card = this._getCardFromHand(owner, cardIndex);
+        this._addCardToBoard(row, card);
+    }
+
+    _moveCardOnBoard(sourceRow, cardIndex, targetRow){
+        const card = this._getCardFromBoard(sourceRow, cardIndex);
+        this._addCardToBoard(targetRow, card);
+    }
+
+    _findCard(cardId){
+        if (this.hand.player1.some((c) => c.id === cardId)) {
+            return "hand-player1";
+        } else if (this.hand.player2.some((c) => c.id === cardId)) {
+            return "hand-player2";
+        } else if (this.board.water.some((c) => c.id === cardId)) {
+            return "water";
+        } else if (this.board.fire.some((c) => c.id === cardId)) {
+            return "fire";
+        } else if (this.board.earth.some((c) => c.id === cardId)) {
+            return "earth";
+        } else if (this.board.air.some((c) => c.id === cardId)) {
+            return "air";
+        } else {
+            return "";
+        }
+    }
+    
+    moveCard(card, targetId) {
+        // request validation (return bool)
+        const isCurrentPlayer = card.owner === this.currentPlayer;
+        if (!isCurrentPlayer) { return false; }
+
+        const isValidTarget = targetId in this.board;
+        if (!isValidTarget) { return false; }
+
+        // determine card position to see if its move from hand or between rows
+        const cardPosition = this._findCard(card.id);
+        if (cardPosition == "") { return false; }
+
+        // move card
+        const isPlayFromHand = cardPosition.includes("hand");
+        if (isPlayFromHand){
+            // play from hand
+            const cardArrayIndex = this.hand[this.currentPlayer].findIndex((c) => c.id === card.id);
+            this._moveCardFromHandToBoard(this.currentPlayer, cardArrayIndex, targetId);
+        } else {
+            // move between rows
+            const cardArrayIndex = this.board[cardPosition].findIndex((c) => c.id === card.id);
+            this._moveCardOnBoard(cardPosition, cardArrayIndex, targetId);
+        }
+        
+        this.updateGameState();
+
+        // success
+        return true;
+    }
+
+    endTurn(player) {
+        // request validation (return bool)
+        const isCurrentPlayer = player === this.currentPlayer;
+        if (!isCurrentPlayer) { return false; }
+        // validation successful
+
+        // combat
+        CombatHelper.startCombat(this);
+
+        // start new turn
+        this._switchCurrentPlayer();
+        this._addCardToHand(this.currentPlayer, this._generateRandomCard());
+        this.updateGameState();
+        return true;
+    }
+    
+    start() {
+        // coinflip
+        const players = ["player1", "player2"];
+        this.currentPlayer = players[Math.floor(Math.random() * players.length)];
+        console.log(`${this.currentPlayer} begins`)
+        
+        // initial draw
+        this.cardCount = 1
+        for (let i = 0; i < 5; i++) {
+            this._addCardToHand(players[0], this._generateRandomCard());
+            this._addCardToHand(players[1], this._generateRandomCard());
+        }
+        
+        this.updateGameState();
     }
     
 }
